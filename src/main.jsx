e284ff9e0f3fb2f05ac35238c54a9aefb5846bcb
Khayam700 <khayamalpood700@gamil.com>
2025-07-14 23:28:17 +0300

    ok

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,9 @@ import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/profile/">
+      <App />
+    </BrowserRouter>
   </StrictMode>
-
 )
